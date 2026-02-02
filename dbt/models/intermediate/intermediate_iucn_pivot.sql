660067837e1f0e46ae1be0dccfc1b0b8eb2d283a
Bénédicte Loriot <loriotbenedicte@gmail.com>
2026-02-02 17:38:06 +0000

    Correct the iucn pivot request Add the join between iucn and ebird table

--- dbt/models/intermediate/intermediate_iucn_pivot.sql
+++ dbt/models/intermediate/intermediate_iucn_pivot.sql
@@ -1,37 +1,15 @@
-WITH global_status AS (
+WITH base AS (
     SELECT
         scientific_name,
-        scopes as region_name,
-        red_list_category_code AS global_status
+        scopes,
+        red_list_category_code AS status
     FROM {{ ref('stg_raw_data__raw_iucn') }}
-    WHERE scopes = 'Global'
-      AND latest_status = true
-),
-regional_status AS (
-    SELECT
-        scientific_name,
-        scopes AS region_name,
-        red_list_category_code AS regional_status
-    FROM {{ ref('stg_raw_data__raw_iucn') }}
-    WHERE scopes != 'Global'
-      AND latest_status = true
+    WHERE latest_status = TRUE
 )
-SELECT 
-    rs.scientific_name,
-    rs.region_name,
-    gs.global_status,
-    rs.regional_status
-FROM regional_status AS rs
-LEFT JOIN global_status AS gs
-    ON rs.scientific_name = gs.scientific_name
-
-UNION ALL
-
-SELECT 
-    scientific_name,
-    scopes AS region_name,
-    red_list_category_code AS global_status,
-    "Global" AS regional_status
-FROM {{ ref('stg_raw_data__raw_iucn') }}
-    WHERE scopes = 'Global'
-      AND latest_status = true
+    SELECT
+        scientific_name,
+        MAX(CASE WHEN scopes = 'Global' THEN status END) AS global_status,
+        MAX(CASE WHEN scopes = 'Europe' THEN status END) AS europe_status,
+        MAX(CASE WHEN scopes = 'Mediterranean' THEN status END) AS mediterranean_status
+    FROM base
+    GROUP BY scientific_name
