17830e37b43ce4dba20ec5375cc81cedea809e43
Bénédicte Loriot <loriotbenedicte@gmail.com>
2026-02-06 19:11:50 +0000

    Add join for iucn and ebird Normalize scientific names Structurate of the intermediate folder

---
+++ dbt/models/intermediate/prepare_join/intermediate_join_matrix_gbiftaxonomy_ebird.sql
@@ -0,0 +1,75 @@
+WITH ebird AS (
+    SELECT 
+        *
+    FROM {{ ref('stg_raw_data__raw_ebird') }}
+),
+sc_name_matrix AS (
+    SELECT 
+        *
+    FROM {{ ref('stg_raw_data__raw_scientificname_matrix') }}
+
+),
+gbif_taxonomy AS (
+    SELECT 
+        *
+    FROM {{ ref('stg_raw_data__raw_gbif_taxonomy') }}
+), 
+normalized AS (
+    SELECT
+        e.sub_id,
+        e.country_code,
+        e.country_name,
+        e.region_name,
+        e.common_name,
+        scm.`order`,
+        scm.family,
+        --- if the corresponding iucn scientific name has not been found, keep the ebird scientific name, else keep icun one. (The species can be called differently)
+        IF(
+            scm.scientific_name= "Not Exist", 
+            e.ebird_scientific_name, 
+            scm.scientific_name
+        ) AS scientific_name,
+        e.observation_date,
+        e.observation_time,
+        e.latitude,
+        e.longitude,
+        e.individual_count,
+        e.is_validated,
+        e.is_reviewed,
+        e.is_location_private
+
+    FROM ebird AS e
+    LEFT JOIN sc_name_matrix AS scm
+    ON e.ebird_scientific_name = scm.ebird_scientific_name
+),
+all_joined AS (
+    SELECT
+        n.sub_id,
+        n.country_code,
+        n.country_name,
+        n.region_name,
+        COALESCE(g.`order`,n.`order`,"Missing data") AS `order`,
+        COALESCE(g.family,n.family,"Missing data") AS family,
+        IF(
+            g.genus IS NULL, 
+            SPLIT(n.scientific_name, ' ')[OFFSET(0)],
+            g.genus
+        ) AS genus,
+        n.common_name,
+        n.scientific_name,
+        n.observation_date,
+        n.observation_time,
+        n.latitude,
+        n.longitude,
+        n.individual_count,
+        n.is_validated,
+        n.is_reviewed,
+        n.is_location_private
+
+    FROM normalized AS n
+    LEFT JOIN gbif_taxonomy AS g
+        ON n.scientific_name = g.scientific_name
+)
+SELECT 
+    *
+FROM all_joined
--- dbt/models/staging/raw_data/stg_raw_data__raw_scientificname_matrix.sql
+++ dbt/models/staging/raw_data/stg_raw_data__raw_scientificname_matrix.sql
@@ -11,7 +11,9 @@ renamed as (
     select
         english_name,
         ebird_scientific_name,
-        scientific_name
+        scientific_name,
+        family,
+        `order`
 
     from source
 
