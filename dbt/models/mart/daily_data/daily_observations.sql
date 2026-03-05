97db3242ad8849c613e958dee4502f4f7483de3d
Bénédicte Loriot <loriotbenedicte@gmail.com>
2026-03-05 16:45:24 +0000

    Changing daily_observation.sql to add LAG and to be able to compare data from the previous day

--- dbt/models/mart/daily_data/daily_observations.sql
+++ dbt/models/mart/daily_data/daily_observations.sql
@@ -1,3 +1,4 @@
+--- Aggregate daily observations by country and IUCN status
 WITH daily_obs AS (
     SELECT
         observation_date
@@ -10,11 +11,35 @@ WITH daily_obs AS (
         ,ROUND(SAFE_DIVIDE(SUM(individual_count),COUNT(DISTINCT sub_id)),0) individuals_per_checklist
         ,SUM(is_threatened_species) AS total_threatened_species_observed
     FROM {{ ref('intermediate_join_ebird_iucn') }}
+    --- Keep only the 2 last days
     WHERE observation_date 
-        BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 8 DAY)
+        BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)
             AND DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
-    GROUP BY observation_date, iucn_global_status, country_code, country_name
+    GROUP BY 
+        observation_date 
+        ,iucn_global_status
+        ,country_code
+        ,country_name
 )
-
+--- Calculate the previous day's metrics for comparison
+, lag_last_day_obs AS (
+    SELECT
+    observation_date
+    ,country_code
+    ,country_name
+    ,iucn_global_status
+    ,total_observers
+    ,total_observations
+    ,total_distinct_species
+    ,individuals_per_checklist
+    ,total_threatened_species_observed
+    --- LAG to get total observations from the previous day
+    ,LAG(total_observations) 
+        OVER (PARTITION BY country_code, country_name, iucn_global_status ORDER BY observation_date ASC) AS last_day_total_observations
+    --- LAG to get individuals per checklists from the previous day
+    ,LAG(individuals_per_checklist) 
+        OVER (PARTITION BY country_code, country_name, iucn_global_status ORDER BY observation_date ASC) AS last_day_total_individual_per_checklist
+    FROM daily_obs
+)
 SELECT *
-FROM daily_obs
+FROM lag_last_day_obs
