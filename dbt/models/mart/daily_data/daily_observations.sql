c1cb2aab31f9953e223f92efc4f0b42ca949ceec
Bénédicte Loriot <loriotbenedicte@gmail.com>
2026-03-05 15:04:31 +0000

    add is_threatened_species in mart/daily_data/daily_observations.sql

--- dbt/models/mart/daily_data/daily_observations.sql
+++ dbt/models/mart/daily_data/daily_observations.sql
@@ -6,6 +6,7 @@ WITH daily_obs AS (
         ,SUM(individual_count) AS total_observations
         ,COUNT(DISTINCT scientific_name) AS total_distinct_species
         ,ROUND(SAFE_DIVIDE(SUM(individual_count),COUNT(DISTINCT sub_id)),0) individuals_per_checklist
+        ,SUM(is_threatened_species) AS total_threatened_species_observed
     FROM {{ ref('intermediate_join_ebird_iucn') }}
     WHERE observation_date 
         BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 8 DAY)
