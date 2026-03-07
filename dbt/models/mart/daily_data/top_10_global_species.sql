5d7138721c12fa8f186f996d9b848f15c682656c
Bénédicte Loriot <loriotbenedicte@gmail.com>
2026-03-07 16:12:15 +0000

    delete top 10 country, the filter is done on looker directly.

--- dbt/models/mart/daily_data/top_10_global_species.sql
+++ dbt/models/mart/daily_data/top_10_global_species.sql
@@ -1,12 +1,16 @@
+--- table with observation of yesterday
 WITH daily_obs AS (
     SELECT
-        scientific_name,
-        common_name,
-        iucn_global_status,
-        SUM(individual_count) AS total_observations
+        country_name
+        ,scientific_name
+        ,common_name
+        ,iucn_global_status
+        ,SUM(individual_count) AS total_observations
     FROM {{ ref('intermediate_join_ebird_iucn') }}
+    --- Keep only observations from yesterday
     WHERE observation_date = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
     GROUP BY
+        country_name,
         scientific_name,
         common_name,
         iucn_global_status
