63ade2981090e177c19b6478564cd9880650c088
BENLRT <73695802+BENLRT@users.noreply.github.com>
2026-03-20 19:40:16 +0100

    Come back to historic version
    Historic API has all the observations for one day
    Adding drop duplicates

--- API_Requests/ebird_requests.py
+++ API_Requests/ebird_requests.py
@@ -23,9 +23,8 @@ credentials = service_account.Credentials.from_service_account_info(credentials_
 df_all=pd.DataFrame()
 
 # calculate yesterday's date
-yesterday = datetime.now(timezone.utc)
-date_str = yesterday.strftime("%Y-%m-%d")
-
+yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+y, m, d = yesterday.year, yesterday.month, yesterday.day
 
 # read country codes from CSV file and make API requests for each country
 df_countries = pd.read_csv("API_Requests/countries.csv", delimiter=';',encoding='utf-8-sig')
@@ -41,44 +40,77 @@ for index, row in df_countries.iterrows():
     country_code=row["ISO-alpha2 Code"]
     country_name=row["Country or Area"]
     region_name = row["Region Name"]
-    url = f"https://api.ebird.org/v2/data/obs/{country_code}/recent"
-    data = None
-    
-    # make GET request to eBird API
-    for attempt in range(3):
-        try:
-            response = requests.get(url, headers=headers)
-            if response.status_code ==200:
-                data = response.json()
-                break  
-                # exit loop if request is successful
-            elif response.status_code >= 500:
-                print(f"Server error {response.status_code} for {country_name} ({country_code}), retrying...")
-                time.sleep(5) # wait for 5 seconds before retrying
-            else:
-                print(f"Error {response.status_code} for {country_name} ({country_code}), not retrying.")
-                break  # exit loop for client errors
-        except requests.exceptions.RequestException as e:
+    # construct API URL for the specific country
+    # US is a special case as we want to get data for each state separately
+    if country_code == "US" or country_code == "CA":
+        dic = dic_us if country_code == "US" else dic_ca
+        for usca_code, usca_name in dic.items():
+            data_usca = None
+            url = f"https://api.ebird.org/v2/data/obs/{usca_code}/historic/{y}/{m}/{d}"
+            for attempt in range(3):
+                try:
+                    response = requests.get(url, headers=headers)
+                    if response.status_code ==200:
+                        data_usca = response.json()
+                        break  
+                    # exit loop if request is successful
+                    elif response.status_code >= 500:
+                        print(f"Server error {response.status_code} for {usca_name} ({usca_code}), retrying...")
+                        time.sleep(5) # wait for 5 seconds before retrying
+                    else:
+                        print(f"Error {response.status_code} for {usca_name} ({usca_code}), not retrying.")
+                        break  # exit loop for client errors
+                except requests.exceptions.RequestException as e:
+                    print(f"Request failed for {usca_name} ({usca_code}): {e}")
+                    continue
+            if not data_usca:
+                print(f"No data available for {usca_name} ({usca_code})")
+                continue
+            for obs in data_usca:
+                obs["countryCode"] = country_code
+                obs["countryName"] = country_name
+                obs["regionName"] = region_name
+            all_data.extend(data_usca)
+            time.sleep(1) # To respect API rate limits
+
+    else:
+        # construct API URL for the specific country
+        data = None
+        url = f"https://api.ebird.org/v2/data/obs/{country_code}/historic/{y}/{m}/{d}"
+        # make GET request to eBird API
+        for attempt in range(3):
+            try:
+                response = requests.get(url, headers=headers)
+                if response.status_code ==200:
+                    data = response.json()
+                    break  
+                    # exit loop if request is successful
+                elif response.status_code >= 500:
+                    print(f"Server error {response.status_code} for {country_name} ({country_code}), retrying...")
+                    time.sleep(5) # wait for 5 seconds before retrying
+                else:
+                    print(f"Error {response.status_code} for {country_name} ({country_code}), not retrying.")
+                    break  # exit loop for client errors
+            except requests.exceptions.RequestException as e:
                 print(f"Request failed for {country_name} ({country_code}): {e}")
                 continue
-        
-    if not data:
-        print(f"No data available for {country_name} ({country_code})")
-        continue
-    for obs in data:
-        obs["countryCode"] = country_code
-        obs["countryName"] = country_name
-        obs["regionName"] = region_name
-    all_data.extend(data)
-    time.sleep(1) # To respect API rate limits
-# Check if any data was collected before attempting to create DataFrame
+
+        if not data:
+            print(f"No data available for {country_name} ({country_code})")
+            continue
+        for obs in data:
+            obs["countryCode"] = country_code
+            obs["countryName"] = country_name
+            obs["regionName"] = region_name
+        all_data.extend(data)
+        time.sleep(1) # To respect API rate limits
+
 if not all_data:
     print("No data collected for any region.")
 else:
     df_all = pd.json_normalize(all_data)
     if df_all.empty:
         print("Empty dataframe after normalization.")
-    # Ensure all expected columns are present, even if they are missing in the API response
     else:
         for col in cols:
             if col not in df_all.columns:
@@ -86,5 +118,6 @@ else:
         # keep only relevant columns : Country Code, Country Name, Common Name, Scientific Name, Observation Date, Latitude, Longitude, How Many, Obs Valid, Obs Reviewed, Location Private, Sub ID
         df_all = df_all[cols]
         df_all = df_all.sort_values("obsDt")
-        df_all = df_all[~df_all['obsDt'].str.startswith(date_str)]
-        pandas_gbq.to_gbq(df_all, "raw_ebird_daily.raw_ebird", project_id="daily-ebird", if_exists="replace", credentials=credentials)
+        # delete duplicates before uploading to BigQuery in case of multiple runs on the same day
+        df_all = df_all.drop_duplicates()
+        pandas_gbq.to_gbq(df_all, "raw_ebird_daily.raw_ebird", project_id="daily-ebird", if_exists="append", credentials=credentials)
